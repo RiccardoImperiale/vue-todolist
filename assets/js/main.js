16e7861f77a9181188db8a0f5c3725f786df17e8
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-13 17:14:12 +0100

    add toggle completed task functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,19 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            newTodo: null,
             tasks: [
                 { text: 'First todo', done: false },
-                { text: 'Second todo', done: false },
+                { text: 'Second todo', done: true },
             ]
         }
     },
     methods: {
-      
+        addTodo() {
+            this.tasks.unshift({ text: this.newTodo, done: false });
+        },
+        toggleDone(index) {
+            this.tasks[index].done = !this.tasks[index].done;
+        }
     }
 }).mount('#app')
